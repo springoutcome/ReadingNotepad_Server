345a96bcfe23ee3f0f13d298e420fc415b371209
Yuuto Harada <b1018185.fun@gmail.com>
2020-03-22 14:41:46 +0900

    mongodbのpathを本番用に変更

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const connectOption = {
     useUnifiedTopology: true,
 };
 
-mongoose.connect('mongodb://localhost/ReadingNotepad_Server', connectOption);
-//mongoose.connect('mongodb://heroku_mmzl4bdb:6bsq28gaf3arclvmpc7dl5qioj@ds139920.mlab.com:39920/heroku_mmzl4bdb', connectOption);
+//mongoose.connect('mongodb://localhost/ReadingNotepad_Server', connectOption);
+mongoose.connect('mongodb://heroku_mmzl4bdb:6bsq28gaf3arclvmpc7dl5qioj@ds139920.mlab.com:39920/heroku_mmzl4bdb', connectOption);
 
 // postデータをjsonで取得できる
 app.use(bodyParser.urlencoded({extended: true}));
